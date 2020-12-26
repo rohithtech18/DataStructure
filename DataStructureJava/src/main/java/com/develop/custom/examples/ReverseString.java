e1158fb67f7c2f21bbd690284772b250ac025ffc
Rohith TS <rohith.tech18@gmail.com>
2020-12-26 14:44:18 +0530

    commit

--- DataStructureJava/src/main/java/com/develop/custom/examples/ReverseString.java
+++ DataStructureJava/src/main/java/com/develop/custom/examples/ReverseString.java
@@ -3,6 +3,8 @@ package com.develop.custom.examples;
 public class ReverseString {
 
 	public String reverseString(String s) {
+		if(s==null)
+			throw new IllegalArgumentException();
 		StringBuffer reverseString= new StringBuffer();
 		for(int i=s.length()-1;i>=0;i--) {
 			reverseString.append(s.charAt(i));
