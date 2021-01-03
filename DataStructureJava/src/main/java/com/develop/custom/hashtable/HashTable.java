d76ff0098f114e137add0ff214399b42d751cf5d
Rohith TS <rohith.tech18@gmail.com>
2021-01-03 11:12:11 +0530

    commit hashtable

---
+++ DataStructureJava/src/main/java/com/develop/custom/hashtable/HashTable.java
@@ -0,0 +1,55 @@
+package com.develop.custom.hashtable;
+
+import java.util.LinkedList;
+
+public class HashTable {
+	
+	private class Entry{
+		
+		private  int key;
+		private String value;
+		
+		public Entry(int key, String value) {
+			this.key = key;
+			this.value = value;
+		}
+	}
+	
+	private LinkedList<Entry>[] entries = new LinkedList[5];
+	
+	public void put(int key,String value) {
+		
+		var index = hash(key);
+		
+		if(entries[index]==null) {
+			entries[index]=new LinkedList<>();
+		}
+		
+		var bucket =entries[index];
+		
+		for(var entry:bucket) {
+			if(entry.key==key) {
+				entry.value=value;
+				return;
+			}
+		}
+		
+ 		bucket.addLast(new Entry(key,value));
+	}
+	
+	public String get(int key) {
+		var index = hash(key);
+		var bucket = entries[index];
+		for(var entry:bucket) {
+			if (entry.key==key) {
+				return entry.value;
+			}
+		}
+		return null;
+	}
+	
+	private int hash(int key) {
+		return key%entries.length;
+	}
+
+}
